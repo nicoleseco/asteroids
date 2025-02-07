64dc75304d1fb05120e8c0f87cb1077e34862703
nicoleseco <nicoleseco@gmail.com>
2025-02-07 17:03:04 -0500

    create play window

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from constants import *
 import pygame
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        black = (0,0,0)
+        screen.fill(black)
+        pygame.display.flip()
     print("Starting asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
     main()
+
+
+
