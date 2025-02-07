24ade51dc8b44e615e3503a4d616ab87d09202f4
nicoleseco <nicoleseco@gmail.com>
2025-02-07 17:14:52 -0500

    circle shapes added

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +16,7 @@ def main():
         black = (0,0,0)
         screen.fill(black)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
